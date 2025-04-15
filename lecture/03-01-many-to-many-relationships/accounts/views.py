f455489fb43f88b762ec656ccbfb4e42471b6711
juheepark <pjhch1009@gmail.com>
2025-04-15 14:13:45 +0900

    250415 lecture update

---
+++ lecture/03-01-many-to-many-relationships/accounts/views.py
@@ -0,0 +1,106 @@
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.shortcuts import redirect, render
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+# Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def delete(request):
+    request.user.delete()
+    return redirect('articles:index')
+
+
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+def change_password(request, user_pk):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
+
+
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+def follow(request, user_pk):
+    User = get_user_model()
+    # 팔로우할 사람 찾기
+    person = User.objects.get(pk=user_pk)
+    # 팔로우할 사람이 나 자신이 아니라면
+    if person != request.user:
+        # 팔로우 리스트에 내(request.user) 가 있으면
+        if request.user in person.followers.all():
+            person.followers.remove(request.user)
+        else:
+            person.followers.add(request.user)
+    # 팔로잉은 보통, 팔로우할 사람의 프로필페이지에서 하는 경우가 많으므로, 상태 프로필페이지로 리다이렉트
+    return redirect('accounts:profile', person.username)
+
